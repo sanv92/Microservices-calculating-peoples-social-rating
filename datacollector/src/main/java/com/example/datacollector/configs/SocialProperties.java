e0624ea7dd06bc4d6564c31ba08e4a8466387d1f
sander.vergeles <sander.vergeles@gmail.com>
2020-04-09 10:18:06 +0300

    Improve configurations

--- datacollector/src/main/java/com/example/datacollector/configs/SocialProperties.java
+++ datacollector/src/main/java/com/example/datacollector/configs/SocialProperties.java
@@ -1,7 +1,13 @@
 package com.example.datacollector.configs;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@Configuration
+@EnableConfigurationProperties(SocialProperties.class)
+@PropertySource("classpath:social.properties")
 @ConfigurationProperties(
 		prefix = "social"
 )
--- datacollector/src/main/java/com/example/datacollector/configs/kafka/KafkaProperties.java
+++ datacollector/src/main/java/com/example/datacollector/configs/kafka/KafkaProperties.java
@@ -1,7 +1,13 @@
 package com.example.datacollector.configs.kafka;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@Configuration
+@EnableConfigurationProperties(KafkaProperties.class)
+@PropertySource("classpath:kafka.properties")
 @ConfigurationProperties(
 		prefix = "kafka"
 )
